37a05e29194fea8314b40e8e7bf0520e6d64e98d
Matthias Kleene <kleene@hm.edu>
2023-11-20 18:02:07 +0100

    add bottom navigator

--- buecherkreisel_flutter/lib/main.dart
+++ buecherkreisel_flutter/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:buecherkreisel_flutter/backend/datatypes.dart';
 import 'package:buecherkreisel_flutter/components/insertation_preview.dart';
+import 'package:buecherkreisel_flutter/screens/explore.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -18,24 +19,96 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: MyHomePage(),
+      home: SizedBox(height: 56, child: KreiselNavigator()),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class KreiselNavigator extends StatefulWidget {
+  const KreiselNavigator({super.key});
+
+  @override
+  State<KreiselNavigator> createState() => _KreiselNavigatorState();
+}
+
+class _KreiselNavigatorState extends State<KreiselNavigator> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Messages',
+      style: optionStyle,
+    ),
+    Explore(),
+    Text(
+      'Add',
+      style: optionStyle,
+    ),
+    Text(
+      'Favorites',
+      style: optionStyle,
+    ),
+    Text(
+      'Account',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Kreisel")),
-      body: Expanded(
-        child: ListView.builder(
-          shrinkWrap: true,
-          scrollDirection: Axis.vertical,
-          itemCount: sampleDataInseration.length,
-          itemBuilder: (c, index) =>
-              InserationPreview(inseration: sampleDataInseration[index]),
-        ),
+      appBar: AppBar(
+        title: const Text('HMKreisel'),
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.message_rounded,
+            ),
+            label: 'Messages',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.map,
+            ),
+            label: 'Explore',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.add,
+            ),
+            label: 'Add',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.favorite,
+            ),
+            label: 'Favorites',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.person,
+            ),
+            label: 'Account',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        unselectedItemColor: Colors.grey,
+        onTap: _onItemTapped,
+        showUnselectedLabels: false,
+        showSelectedLabels: false,
       ),
     );
   }
