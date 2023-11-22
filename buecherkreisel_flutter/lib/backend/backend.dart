8db939d48b851b1ead908dd9ddf86de65c515549
Sreeroop <adukkadu@hm.edu>
2023-11-22 19:56:35 +0100

    refactor

--- buecherkreisel_flutter/lib/backend/backend.dart
+++ buecherkreisel_flutter/lib/backend/backend.dart
@@ -1,13 +1,19 @@
+import 'package:buecherkreisel_flutter/backend/datatypes.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-class ApiClient {
-  final String baseUrl =
-      'https://your-api-base-url.com'; // Replace with your API base URL
+class APIClient {
+  // use IP 10.0.2.2 to access localhost from windows client
+  //static const baseUrl = "http://127.0.0.1:8080/";
 
+  // use IP 10.0.2.2 to access localhost from emulator!
+  static const baseUrl = "http://10.0.2.2:8080/";
+
+  // GET
   Future<dynamic> fetchData(String endpoint) async {
-    final response = await http.get(Uri.parse('$baseUrl/$endpoint'));
+    final response = await http.get(Uri.parse('$baseUrl$endpoint'));
 
+    // check response from backend
     if (response.statusCode == 200) {
       return json.decode(response.body);
     } else {
@@ -15,9 +21,10 @@ class ApiClient {
     }
   }
 
+  // POST
   Future<dynamic> postData(String endpoint, dynamic data) async {
     final response = await http.post(
-      Uri.parse('$baseUrl/$endpoint'),
+      Uri.parse('$baseUrl$endpoint'),
       body: json.encode(data),
       headers: {'Content-Type': 'application/json'},
     );
@@ -29,9 +36,10 @@ class ApiClient {
     }
   }
 
+  // UPDATE
   Future<dynamic> updateData(String endpoint, dynamic data) async {
     final response = await http.put(
-      Uri.parse('$baseUrl/$endpoint'),
+      Uri.parse('$baseUrl$endpoint'),
       body: json.encode(data),
       headers: {'Content-Type': 'application/json'},
     );
@@ -43,8 +51,9 @@ class ApiClient {
     }
   }
 
+  // DELETE
   Future<void> deleteData(String endpoint) async {
-    final response = await http.delete(Uri.parse('$baseUrl/$endpoint'));
+    final response = await http.delete(Uri.parse('$baseUrl$endpoint'));
 
     if (response.statusCode != 204) {
       throw Exception('Failed to delete data');
