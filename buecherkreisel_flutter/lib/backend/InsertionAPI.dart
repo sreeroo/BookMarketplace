64296a2ef100cba7f2d66b168c01a4508aef6bd0
Sreeroop <adukkadu@hm.edu>
2023-11-25 23:16:43 +0100

    reformat

--- buecherkreisel_flutter/lib/backend/InsertionAPI.dart
+++ buecherkreisel_flutter/lib/backend/InsertionAPI.dart
@@ -5,13 +5,7 @@ import 'backend.dart';
 class InsertionAPI {
   final restAPI = APIClient();
 
-  // get list of all insertions from the backend
-  Future<List<Insertion>> getAllInsertions(http.Client client) async {
-    final response = await restAPI.fetchData(client, 'insertions');
-    return List<Insertion>.from(response.map((e) => Insertion.fromJson(e)));
-  }
-
-  // save a new Insertion on the backend
+  // CREATE a new Insertion on the backend
   Future<Insertion> createInsertion(
       http.Client client, Insertion insertion) async {
     final response =
@@ -19,7 +13,20 @@ class InsertionAPI {
     return Insertion.fromJson(response);
   }
 
-  // update an existing Insertion on the backend
+  // READ list of all insertions from the backend
+  Future<List<Insertion>> getAllInsertions(http.Client client) async {
+    final response = await restAPI.fetchData(client, 'insertions');
+    return List<Insertion>.from(response.map((e) => Insertion.fromJson(e)));
+  }
+
+  // READ a specific Insertion from the backend
+
+  Future<Insertion> getInsertionById(http.Client client, String id) async {
+    final response = await restAPI.fetchData(client, 'insertions/$id');
+    return Insertion.fromJson(response);
+  }
+
+  // UPDATE an existing Insertion on the backend
   Future<Insertion> updateInsertion(
       http.Client client, Insertion insertion) async {
     final response = await restAPI.updateData(
@@ -27,16 +34,9 @@ class InsertionAPI {
     return Insertion.fromJson(response);
   }
 
-  // delete an existing Insertion on the backend
+  // DELETE an existing Insertion on the backend
 
   Future<void> deleteInsertion(http.Client client, Insertion insertion) async {
     await restAPI.deleteData(client, 'insertions/${insertion.id}');
-  }
-
-  // get a specific Insertion from the backend
-
-  Future<Insertion> getInsertionById(http.Client client, String id) async {
-    final response = await restAPI.fetchData(client, 'insertions/$id');
-    return Insertion.fromJson(response);
   }
 }
