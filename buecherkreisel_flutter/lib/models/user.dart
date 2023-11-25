d63224a6c035d95b1e0b3744c02180e5885218c2
Sreeroop <adukkadu@hm.edu>
2023-11-25 23:34:38 +0100

    refactor

--- buecherkreisel_flutter/lib/models/user.dart
+++ buecherkreisel_flutter/lib/models/user.dart
@@ -3,20 +3,20 @@ typedef user_id = String;
 class User {
   user_id id;
   String username;
-  String imageURL;
-  User({required this.id, required this.imageURL, required this.username});
+  String imageURI;
+  User({required this.id, required this.imageURI, required this.username});
 
   // parse Item from JSON-data
   factory User.fromJson(Map<String, dynamic> json) => User(
         id: json['id'],
-        imageURL: json['imageURL'],
+        imageURI: json['imageURL'],
         username: json['username'],
       );
 
   // map item to JSON-data
   Map<String, dynamic> toJson() => {
         "id": id,
-        "imageURL": imageURL,
+        "imageURL": imageURI,
         "username": username,
       };
 }
