1b75904da2c15a91fbb40c0e69aef7e21ad5e8b7
Sven Lange <sven.lange@hm.edu>
2023-12-06 09:19:10 +0100

    Get Request Test Listing Controller

--- spring/src/test/java/edu/hm/cs/buecherkreisel/spring/ListingControllerTest.java
+++ spring/src/test/java/edu/hm/cs/buecherkreisel/spring/ListingControllerTest.java
@@ -1,18 +1,22 @@
 package edu.hm.cs.buecherkreisel.spring;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Base64;
+import java.util.Optional;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.web.multipart.MultipartFile;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -22,10 +26,15 @@ class ListingControllerTest {
     MockMvc mockMvc;
 
     @Autowired
-    private ListingRepository listingRepository;
+    ApplicationContext context;
 
     @Autowired
     private UserRepository userRepository;
+
+    @BeforeEach
+    public void setup() {
+        userRepository.deleteAll();
+    }
 
     @Test
     public void testCreateListing() throws Exception {
@@ -35,7 +44,7 @@ class ListingControllerTest {
 
         mockMvc.perform(MockMvcRequestBuilders
                         .multipart("/listings")
-                        .file("images", "Bilder".getBytes())
+                        .file("images", Base64.getDecoder().decode("Bild"))
                         .param("title", "Titel")
                         .param("price", "29.99")
                         .param("category", "INFORMATIK")
@@ -49,13 +58,80 @@ class ListingControllerTest {
     }
 
     @Test
+    public void testCreateListingUserNotFound() throws Exception {
+
+        UserRepository userRepository = context.getBean(UserRepository.class);
+
+        User user = userRepository.save(
+                new User("Nutzername", "Password", "Profilbild"));
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .multipart("/listings")
+                        .file("images", Base64.getDecoder().decode("Bild"))
+                        .param("title", "Titel")
+                        .param("price", "29.99")
+                        .param("category", "INFORMATIK")
+                        .param("offersDelivery", "true")
+                        .param("description", "Beschreibung")
+                        .param("isReserved", "false")
+                        .param("user_id", "2")
+                        .param("location", "München")
+                        .param("token", user.getToken())
+                ).andExpect(status().is(401));
+    }
+
+    private MvcResult createListing(User user) throws Exception {
+        return mockMvc.perform(MockMvcRequestBuilders
+                        .multipart("/listings")
+                        .file("images", Base64.getDecoder().decode("Bild"))
+                        .param("title", "Titel")
+                        .param("price", "29.99")
+                        .param("category", "INFORMATIK")
+                        .param("offersDelivery", "true")
+                        .param("description", "Beschreibung")
+                        .param("isReserved", "false")
+                        .param("user_id", String.valueOf(user.getId()))
+                        .param("location", "München")
+                        .param("token", user.getToken())
+                ).andReturn();
+    }
+
+    @Test
     public void testGetListing() throws Exception {
 
-        // TODO Body überprüfen
+        User user = userRepository.save(
+                new User("Nutzername", "Password", "Profilbild"));
 
-        mockMvc.perform(MockMvcRequestBuilders.get("/listings"))
-                .andDo(print())
-                .andExpect(status().isOk());
+        MvcResult mvcResult = createListing(user);
+        if (mvcResult.getResponse().getStatus() != 200) {
+            throw new RuntimeException("Unable to create Listing!");
+        }
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/listings")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").exists())
+                .andExpect(jsonPath("$[0].id").exists())
+                .andExpect(jsonPath("$[0].id").value("1"))
+                .andExpect(jsonPath("$[0].title").exists())
+                .andExpect(jsonPath("$[0].title").value("Titel"))
+                .andExpect(jsonPath("$[0].price").exists())
+                .andExpect(jsonPath("$[0].price").value("29.99"))
+                .andExpect(jsonPath("$[0].category").exists())
+                .andExpect(jsonPath("$[0].category").value("INFORMATIK"))
+                .andExpect(jsonPath("$[0].offersDelivery").exists())
+                .andExpect(jsonPath("$[0].offersDelivery").value("true"))
+                .andExpect(jsonPath("$[0].description").exists())
+                .andExpect(jsonPath("$[0].description").value("Beschreibung"))
+                .andExpect(jsonPath("$[0].reserved").exists())
+                .andExpect(jsonPath("$[0].reserved").value("false"))
+                .andExpect(jsonPath("$[0].userID").exists())
+                .andExpect(jsonPath("$[0].userID").value(user.getId()))
+                .andExpect(jsonPath("$[0].location").exists())
+                .andExpect(jsonPath("$[0].location").value("München"))
+                .andExpect(jsonPath("$[0].images").exists())
+                .andExpect(jsonPath("$[0].images[0]").value("Bild"));
+
     }
 
 }
