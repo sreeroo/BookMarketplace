e305266f299c066376276bf16c1696bf82a855e2
Sven Lange <sven.lange@hm.edu>
2023-12-19 09:52:10 +0100

    Weitere Kategorien im Backend hinzugefügt.

--- spring/src/main/java/edu/hm/cs/buecherkreisel/spring/Category.java
+++ spring/src/main/java/edu/hm/cs/buecherkreisel/spring/Category.java
@@ -2,7 +2,13 @@ package edu.hm.cs.buecherkreisel.spring;
 
 public enum Category {
     INFORMATIK("Informatik"),
-    MASCHINENBAU("Maschinenbau");
+    MASCHINENBAU("Maschinenbau"),
+    ELEKTROTECHNIK("Elektrotechnik"),
+    MATHEMATIK("Mathematik"),
+    PHYSIK("Physik"),
+    WIRTSCHAFT("Wirtschaft"),
+    POLITIK("Politik"),
+    SONSTIGES("Sonstiges");
 
     public final String category;
 
