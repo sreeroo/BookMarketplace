f537adc1b109dd086d3b184a8dd5967fc5f745c5
Erik Bittner <bittner.erik@hm.edu>
2024-01-13 02:43:07 +0100

    Changes to Favorite Screen to fetch data correctly using API

--- buecherkreisel_flutter/lib/screens/favorites.dart
+++ buecherkreisel_flutter/lib/screens/favorites.dart
@@ -1,30 +1,57 @@
 import 'package:buecherkreisel_flutter/backend/datatypes.dart';
 import 'package:flutter/material.dart';
+import 'package:buecherkreisel_flutter/models/listing.dart';
 import 'package:provider/provider.dart';
 
 import '../components/listing_preview.dart';
 
-class Favorites extends StatelessWidget {
-  const Favorites({super.key});
+class Favorites extends StatefulWidget{
+  @override
+  FavoriteState createState() => FavoriteState();
+
+  const Favorites({Key? key}) : super(key: key); 
+}
+
+class FavoriteState extends State<Favorites> {
+
+  @override
+  void initState() {
+    super.initState();
+    scheduleLikedListingsFetch();
+  }
+
+  void scheduleLikedListingsFetch() {
+    // Schedule a callback for the next event loop iteration
+    Future.delayed(Duration.zero, fetchLikedListings);
+  }
+
+  Future<void> fetchLikedListings() async {
+    final appState = Provider.of<AppState>(context, listen: false);
+    await appState.listingState.getLikedListings(appState.user.likedListings);
+    // Call setState if you need to update the UI after fetching data
+    if (mounted) {
+      setState(() {});
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Consumer2<ListingState, AppState>(builder: (
-        c, listingState, appState, w) {
+    return Consumer<AppState>(builder: (  
+        c, appState, w) {
 
       if (appState.user.token.isEmpty) {
         return const Center(
               child: Text("Login to see your favorites"));
       }
 
-      return listingState.likedListings.isEmpty
+      return appState.listingState.liked_listings.isEmpty
           ? const Center(
               child: Text("You don't have any favorites yet"))
           : ListView.builder(
-              itemCount: listingState.likedListings.length,
+              itemCount: appState.listingState.liked_listings.length,
               itemBuilder: (context, index) {
                 return ListingPreview(
-                  listing: listingState.likedListings.elementAt(index),
+                  listing: appState.listingState.liked_listings.elementAt(index),
                 );
               },
             );
